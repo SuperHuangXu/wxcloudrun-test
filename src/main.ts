80bb0ea03465b5c6effdc722f9727fc746ea2565
SuperHuangXu <www.1567891@qq.com>
2021-12-27 17:54:07 +0800

    fix: 🐛 启动方法

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const port = 3000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.listen(port, () => {
+    console.log('服务启动成功，端口：', port);
+  });
 }
 bootstrap();
